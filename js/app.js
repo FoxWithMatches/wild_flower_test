b0a9db7f7cf3145e54bdd8c754f3018eb274b21a
FoxWithMatches <freiheiit92@mail.ru>
2023-02-21 13:13:48 +0300

    modal

--- js/app.js
+++ js/app.js
@@ -20,4 +20,31 @@ $(function () {
     nav.toggleClass("show");
     social.toggleClass("show");
 });
+
+//modal
+
+const modalCall = $("[data-modal]");
+const modalClose = $("[data-close]");
+
+
+
+modalCall.on("click", function(event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data('modal');
+
+    $("#modal").addClass("show");
+    $("body").addClass("no-scroll");
+})
+
+modalClose.on("click", function(event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data('modal');
+
+    $("#modal").removeClass("show")
+    $("body").removeClass("no-scroll");
+})
 });
